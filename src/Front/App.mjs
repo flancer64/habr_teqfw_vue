fe4a4dbda43870ea621aa3fb626ab2bb947e10aa
Alex Gusev <alex@flancer64.com>
2021-12-19 11:43:41 +0200

    DC-1 Setup initial project

--- src/Front/App.mjs
+++ src/Front/App.mjs
@@ -8,117 +8,109 @@
 const NS = 'Fl64_Habr_Vue_Front_App';
 
 export default class Fl64_Habr_Vue_Front_App {
-    /** @type {Fl64_Habr_Vue_Front_Defaults} */
-    #DEF;
-    /** @type {TeqFw_Di_Shared_Container} */
-    #container;
-    /** @type {TeqFw_Web_Front_Model_Config} */
-    #config;
-    /** @type {TeqFw_I18n_Front_Lib} */
-    #I18nLib;
-    /** @type {TeqFw_Vue_Front_Lib} */
-    #VueLib;
-    /** @type {TeqFw_Ui_Quasar_Front_Lib} */
-    #QuasarLib;
-    /** @type {Fl64_Habr_Vue_Front_Layout_Base} */
-    #layoutBase;
-    #root; // root vue component for the application
-
     constructor(spec) {
-        this.#config = spec['TeqFw_Web_Front_Model_Config$'];
-        this.#container = spec['TeqFw_Di_Shared_Container$'];
-        this.#DEF = spec['Fl64_Habr_Vue_Front_Defaults$'];
-        this.#I18nLib = spec['TeqFw_I18n_Front_Lib$'];
-        this.#layoutBase = spec['Fl64_Habr_Vue_Front_Layout_Base$'];
-        this.#QuasarLib = spec['TeqFw_Ui_Quasar_Front_Lib$'];
-        this.#VueLib = spec['TeqFw_Vue_Front_Lib$'];
-    }
+        const {createApp} = spec['TeqFw_Vue_Front_Lib_Vue'];
+        const {createRouter, createWebHashHistory} = spec['TeqFw_Vue_Front_Lib_Router'];
+        /** @type {Fl64_Habr_Vue_Front_Defaults} */
+        const _DEF = spec['Fl64_Habr_Vue_Front_Defaults$'];
+        /** @type {TeqFw_Di_Shared_Container} */
+        const _container = spec['TeqFw_Di_Shared_Container$'];
+        /** @type {TeqFw_Web_Front_Model_Config} */
+        const _config = spec['TeqFw_Web_Front_Model_Config$'];
+        /** @type {TeqFw_I18n_Front_Lib} */
+        const _I18nLib = spec['TeqFw_I18n_Front_Lib$'];
+        /** @type {Fl64_Habr_Vue_Front_Layout_Base} */
+        const _layoutBase = spec['Fl64_Habr_Vue_Front_Layout_Base$'];
+        /** @type {TeqFw_Ui_Quasar_Front_Lib} */
+        const _quasar = spec['TeqFw_Ui_Quasar_Front_Lib'];
 
-    /**
-     * Initialize application.
-     *
-     * @return {Promise<void>}
-     */
-    async init() {
-        // DEFINE INNER FUNCTIONS
+        // DEFINE WORKING VARS / PROPS
+        let _root; // root vue component for the application
+
+        // DEFINE INSTANCE METHODS
 
         /**
-         * Setup working languages and fallback language and add translation function to the Vue.
+         * Initialize application.
          *
-         * @param {Object} app
-         * @param {TeqFw_I18n_Front_Lib} I18nLib
          * @return {Promise<void>}
-         * @memberOf Fl64_Habr_Vue_Front_App.init
          */
-        async function initI18n(app, I18nLib) {
-            await I18nLib.init(['en', 'ru'], 'en');
-            const appProps = app.config.globalProperties;
-            const i18n = I18nLib.getI18n();
-            // add translation function to Vue
-            appProps.$t = function (key, options) {
-                // add package name if namespace is omitted in the key
-                const ns = this.$options.teq?.package;
-                if (ns && key.indexOf(':') <= 0) key = `${ns}:${key}`;
-                return i18n.t(key, options);
+        this.init = async function () {
+            // DEFINE INNER FUNCTIONS
+
+            /**
+             * Setup working languages and fallback language and add translation function to the Vue.
+             *
+             * @param {Object} app
+             * @param {TeqFw_I18n_Front_Lib} I18nLib
+             * @return {Promise<void>}
+             * @memberOf Fl64_Habr_Vue_Front_App.init
+             */
+            async function initI18n(app, I18nLib) {
+                await I18nLib.init(['en', 'ru'], 'en');
+                const appProps = app.config.globalProperties;
+                const i18n = I18nLib.getI18n();
+                // add translation function to Vue
+                appProps.$t = function (key, options) {
+                    // add package name if namespace is omitted in the key
+                    const ns = this.$options.teq?.package;
+                    if (ns && key.indexOf(':') <= 0) key = `${ns}:${key}`;
+                    return i18n.t(key, options);
+                }
             }
+
+            function initQuasarUi(app, quasar) {
+                app.use(quasar, {config: {}});
+                quasar.iconSet.set(quasar.iconSet.svgMaterialIcons);
+            }
+
+            function initRouter(app, DEF, container) {
+                /** @type {{addRoute}} */
+                const router = createRouter({
+                    history: createWebHashHistory(),
+                    routes: [],
+                });
+                // setup application routes (load es6-module on demand with DI-container)
+                router.addRoute({
+                    path: DEF.ROUTE_HOME,
+                    component: () => container.get('Fl64_Habr_Vue_Front_Route_Home$')
+                });
+                router.addRoute({
+                    path: DEF.ROUTE_CFG,
+                    component: () => container.get('Fl64_Habr_Vue_Front_Route_Cfg$')
+                });
+                //
+                app.use(router);
+            }
+
+            // MAIN FUNCTIONALITY
+
+            // create root vue component
+            _root = createApp({
+                teq: {package: _DEF.NAME},
+                name: NS,
+                template: '<router-view/>',
+            });
+            // ... and add global available components
+            _root.component('LayoutBase', _layoutBase);
+
+            // other initialization
+            await _config.init({}); // this app has no separate 'doors' (entry points)
+            await initI18n(_root, _I18nLib);
+            initQuasarUi(_root, _quasar);
+            initRouter(_root, _DEF, _container);
         }
 
-        function initQuasarUi(app, QuasarLib) {
-            const quasar = QuasarLib.getQuasar()
-            app.use(quasar, {config: {}});
-            quasar.iconSet.set(quasar.iconSet.svgMaterialIcons);
+        /**
+         * Mount root vue component of the application to DOM element.
+         *
+         * @see https://v3.vuejs.org/api/application-api.html#mount
+         *
+         * @param {Element|string} elRoot
+         * @return {Object} the root component instance
+         */
+        this.mount = function (elRoot) {
+            return _root.mount(elRoot);
         }
-
-        function initRouter(app, VueLib, DEF, container) {
-            /** @type {{createRouter, createWebHashHistory}} */
-            const Router = VueLib.getRouter();
-            /** @type {{addRoute}} */
-            const router = Router.createRouter({
-                history: Router.createWebHashHistory(),
-                routes: [],
-            });
-            // setup application routes (load es6-module on demand with DI-container)
-            router.addRoute({
-                path: DEF.ROUTE_HOME,
-                component: () => container.get('Fl64_Habr_Vue_Front_Route_Home$')
-            });
-            router.addRoute({
-                path: DEF.ROUTE_CFG,
-                component: () => container.get('Fl64_Habr_Vue_Front_Route_Cfg$')
-            });
-            //
-            app.use(router);
-        }
-
-        // MAIN FUNCTIONALITY
-
-        // create root vue component
-        /** @type {{createApp}} */
-        const Vue = this.#VueLib.getVue();
-        this.#root = Vue.createApp({
-            teq: {package: this.#DEF.NAME},
-            name: NS,
-            template: '<router-view/>',
-        });
-        // ... and add global available components
-        this.#root.component('LayoutBase', this.#layoutBase);
-
-        // other initialization
-        await this.#config.init({}); // this app has no separate 'doors' (entry points)
-        await initI18n(this.#root, this.#I18nLib);
-        initQuasarUi(this.#root, this.#QuasarLib);
-        initRouter(this.#root, this.#VueLib, this.#DEF, this.#container);
     }
 
-    /**
-     * Mount root vue component of the application to DOM element.
-     *
-     * @see https://v3.vuejs.org/api/application-api.html#mount
-     *
-     * @param {Element|string} elRoot
-     * @return {Object} the root component instance
-     */
-    mount(elRoot) {
-        return this.#root.mount(elRoot);
-    }
 }
--- src/Front/Model/Lang.mjs
+++ src/Front/Model/Lang.mjs
@@ -5,9 +5,8 @@ export default class Fl64_Habr_Vue_Front_Model_Lang {
     #data;
 
     constructor(spec) {
-        /** @type {TeqFw_Vue_Front_Lib} */
-        const VueLib = spec['TeqFw_Vue_Front_Lib$'];
-        this.#data = VueLib.getVue().ref(0);
+        const {ref} = spec['TeqFw_Vue_Front_Lib_Vue'];
+        this.#data = ref(0);
     }
 
     getData() {
